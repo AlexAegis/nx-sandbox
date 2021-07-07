0bd0f0c0de68ade873fe20ed6a8b2b20c56df15f
AlexAegis <alexaegis@gmail.com>
2021-07-07 20:13:53 +0200

    feat: added swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,14 +6,29 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter({ logger: true })
+	);
 	const globalPrefix = 'api';
 	app.setGlobalPrefix(globalPrefix);
 	const port = process.env.PORT || 3333;
+
+	const config = new DocumentBuilder()
+		.setTitle('Todo Api')
+		.setDescription('The Todo API description')
+		.setVersion('1.0')
+		.addTag('todo')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('/', app, document);
+
 	await app.listen(port, () => {
 		Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
 	});
