965f0e20c8095bde0c904b008a4d92b621afefcf
AlexAegis <alexaegis@gmail.com>
2021-07-07 20:09:41 +0200

    chore: format

--- apps/todo/src/app/app.component.ts
+++ apps/todo/src/app/app.component.ts
@@ -3,11 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { Message } from '@nx-sandbox/api-interfaces';
 
 @Component({
-  selector: 'nx-sandbox-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	selector: 'nx-sandbox-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+	hello$ = this.http.get<Message>('/api/hello');
+	constructor(private http: HttpClient) {}
 }
